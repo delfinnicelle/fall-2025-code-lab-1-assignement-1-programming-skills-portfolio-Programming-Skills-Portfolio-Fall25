2f819a4c6445a9bb0dc3a0dd7db47e727a628300
delfinnicelle <delfin.nicelle@gmail.com>
2025-11-21 19:56:34 +0400

    exercise 2 - simple sums
    adds two integers using three variables, the third variable storing the sum before being printed.

---
+++ Exercise 02- Simple Sums/exercise2-sums.py
@@ -0,0 +1,6 @@
+num1 = 8 # declares the variable num1 with the integer 8
+num2 = 10 # declares the variable num2 with the integer 10
+
+sum = num1 + num2 # declares the variable sum and adds the two integers stored in the two variables
+
+print(sum) # prints the sum stored
