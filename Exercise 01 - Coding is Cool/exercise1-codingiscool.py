284840595bd799a4752f207cf73fb9423c31f9f0
delfinnicelle <delfin.nicelle@gmail.com>
2025-11-21 19:50:34 +0400

    exercise 1 - coding is cool
    The code prints out the phrase "Coding is Cool" using string concatenation and variables.

---
+++ Exercise 01 - Coding is Cool/exercise1-codingiscool.py
@@ -0,0 +1,8 @@
+word1 = "Coding" # stores the word "Coding" under the variable name 'word1'
+word2 = "is" # stores the word "is" under the variable name 'word2'
+word3 = "Cool" # stores the word "is" under the variable name 'word3'
+
+phrase = word1 + " " + word2 + " " + word3 #adds the three variables along with spaces between.
+
+print(phrase) # prints out the phrase
+
