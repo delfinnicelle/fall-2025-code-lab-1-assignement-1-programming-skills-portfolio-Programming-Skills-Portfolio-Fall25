49d8bfc80e6234eaaca5f81f6a756d3291661b0d
delfinnicelle <delfin.nicelle@gmail.com>
2025-11-22 18:27:42 +0400

    exercise 9 - hello
    prints hello to the console using functions.

---
+++ Exercise 09 -Hello/exercise9-hello.py
@@ -0,0 +1,8 @@
+def hello() : # defines the function "hello()"
+    print("Hello") # prints out the string "Hello"
+
+def main() :  # defines the main function
+    hello() # calls the hello function to print "Hello" to the console
+
+if __name__ == "__main__" : 
+    main()
