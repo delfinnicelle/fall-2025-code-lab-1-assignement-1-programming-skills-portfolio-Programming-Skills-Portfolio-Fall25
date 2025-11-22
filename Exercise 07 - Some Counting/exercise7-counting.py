7bc3997735ac3589bf44d453f7ebbe4e1801fa6b
delfinnicelle <delfin.nicelle@gmail.com>
2025-11-22 18:25:38 +0400

    exercise 7 - counting
    uses for loop to count in increments or decrements using a range of given values.

---
+++ Exercise 07 - Some Counting/exercise7-counting.py
@@ -0,0 +1,34 @@
+# a loop that counts up from 0 to 50 in increments of 1.
+
+for x in range(51) : # range starts at 0 and ends before 51, increments of 1
+  print(x)
+
+print(" ") # prints an empty line to separate the output of the loops.
+
+# a loop that counts down from 50 to 0 in decrements of 1.
+
+for x in range(50, -1, -1) : # range starts at value 50 and ends before the value -1, decrements of 1
+  print(x)
+
+print(" ") # prints an empty line to separate the output of the loops.
+
+# a loop that counts up from 30 to 50 in increments of 1.
+
+for x in range(30, 51 ) : # range starts at value 30 and ends before the value 51, increments of 1
+  print(x)
+
+print(" ") # prints an empty line to separate the output of the loops.
+
+# a loop that counts down from 50 to 10 in decrements of 2.
+
+for x in range(50, 9, -2 ) : # range starts at value 50 and ends before the value 9, decrements of 2
+  print(x)
+
+print("") # prints an empty line to separate the output of the loops.
+
+# a loop that counts up from 100 to 200 in increments of 5.
+
+for x in range(100, 201, 5) : # range starts at value 100 and ends before the value 201, increments of 5
+  print(x)
+
+print(" ") # prints an empty line to separate the output of the loops.
